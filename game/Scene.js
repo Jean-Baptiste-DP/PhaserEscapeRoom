a01ecab73a5fbe4c7324ec9509290bf7745497c0
Jean-Baptiste-DP <Jean-Baptiste-DP@users.noreply.github.com>
2023-04-01 17:17:23 +0000

    deploy: 14eccd61647aa43d02320e584c6f11b7bdea1479

--- game/Scene.js
+++ game/Scene.js
@@ -8,51 +8,39 @@ class Scene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet("player", "images/character/players.png", { frameWidth: 40, frameHeight: 50 })
-        this.load.image("transparency", "images/character/transparent.png");
-        this.load.atlas(
-            "flares",
-            "images/particles/flares.png",
-            "images/particles/flares.json"
-        );
-        this.load.image("dirt", "images/block/dirt.png");
+        this.load.spritesheet("player", "images/character/players.png", {
+            frameWidth: 40,
+            frameHeight: 50,
+        });
         this.load.tilemapTiledJSON("map", "images/map/maptile.json");
         this.load.image("blocktiles", "images/map/tile.png");
-        this.load.image("decorations", "images/map/decors.png")
+        this.load.image("decorations", "images/map/decors.png");
     }
 
     create() {
+        // initialise the map
         const map = this.make.tilemap({ key: "map" });
+
+        // maptiles
         const blocktileset = map.addTilesetImage("blocktiles");
         const decorations = map.addTilesetImage("decorations");
-        const BackgroundLayer = map.createLayer("BackgroundLayer", blocktileset);
-        map.createLayer("DecorationLayer", decorations)
-        // const DecorationsLayer = map.createLayer("Decors", decorations)
+
+        //layers
+        map.createLayer("BackgroundLayer", blocktileset);
+        map.createLayer("DecorationLayer", decorations);
         const Groundlayer = map.createLayer("GroundLayer", blocktileset);
 
+        //collision on groundlayer
         map.setCollisionByProperty({ collides: true });
         this.matter.world.convertTilemapLayer(Groundlayer);
-        // this.matter.world.convertTilemapLayer(BackgroundLayer);
 
-        Groundlayer.setCollisionByExclusion([0]);
-
+        // input cursor
         this.cursors = this.input.keyboard.createCursorKeys();
-        // var particles = this.add.particles('flares');
 
-        // var emitter = particles.createEmitter({
-        //     frame: 'yellow',
-        //     lifespan: 1000,
-        //     speed: { min: 100, max: 200 },
-        //     gravityY: 100,
-        //     angle: 270,
-        //     scale: { start: 0.4, end: 0 },
-        //     quantity: 0.5,
-        //     blendMode: 'ADD'
-        // });
-
+        // player creation
         this.player = new Player(this, Phaser.Physics.Matter.Matter);
 
-        // this.matter.world.on('beforeupdate', this.player.resetTouching)
+        // event on touch, and before/after updates
         this.matter.world.on("collisionactive", (event) => {
             this.player.collisionStart(event);
         });
@@ -65,24 +53,9 @@ class Scene extends Phaser.Scene {
     }
 
     update() {
+        //call moving function of the player
         this.player.moveSprite(this.cursors);
     }
 }
 
-function logTouching(number) {
-    if (number % 4 == 0) {
-        console.log("Touching Bottom");
-    } else if (number % 4 == 1) {
-        console.log("Touching Left");
-    } else if (number % 4 == 2) {
-        console.log("Touching Top");
-    } else {
-        console.log("Touching Right");
-    }
-}
-
 export default Scene;
-
-const getAttributes = (obj) => {
-    return Object.getOwnPropertyNames(obj);
-};
